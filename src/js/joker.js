5bc6181a2df1d61d1d88d639f0df232fdae04a1f
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-10-22 14:51:17 +0200

    importing random joke

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,2 +1,7 @@
 import joker from "give-me-a-joke";
-joker.getRandomDadJoke(console.log);
+
+export function dadJoke() {
+    return new promise(function(resolve, reject){
+        joker.getRandomDadJoke(resolve)
+    })
+}
